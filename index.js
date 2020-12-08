2e72336bed108b5128e42c776a0d1819014da2b9
Jeff-Dev88 <jeffoliveira71@gmail.com>
2020-12-07 22:16:08 -0300

    resolução do ex.1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const eplusDevList = ['Parça', 'Galego', 'Xará', 'Baqueta', 'Na', 'Lindo', 'Pablito', 'Bahia']
 
-function listDevs(list){
-
+function listDevs(){
+	eplusDevList.forEach(function(dev){
+  console.log(dev);
+})
 }
 
 listDevs(eplusDevList)
-
 //a função deve dar console.log em cada um dos desenvolvedores da e-plus.
